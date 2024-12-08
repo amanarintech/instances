c0bdc5085138597442cb07a6b014f22e48dd8022
Aman-Parsad <amanparsad543@gmail.com>
2024-12-08 20:04:47 +0530

    ok

--- main.tf
+++ main.tf
@@ -39,15 +39,15 @@ tags = {
     depends_on = [module.webserver]
 }
 
-#module "web_docker_host" {
+#  module "web_docker_host" {
 #  source = "./modules/linux_node"
 #  instance_count = "1"
-#  ami = "ami-053b12d3152c0cc71"
+#  ami = "ami-0e2c8caa4b6378d8c"
 #  instance = "t2.micro"
 #  key_name = data.terraform_remote_state.network_details.outputs.key_name
 #  subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
 #  vpc_security_group_ids = data.terraform_remote_state.network_details.outputs.security_group_id_array
-#tags = {
+#  tags = {
 #    Name = var.web_docker_host_prefix
 #  }
 #    install_package = "dockerhost"
